d916fa9d3b984b160d10fce615c31e91bf339443
abin0001 <abinj2003@gamail.com>
2024-02-12 03:47:01 +0530

    Back-end-Complete

--- Elvy.py
+++ Elvy.py
@@ -8,61 +8,58 @@ import google.ai.generativelanguage as glm
 from dotenv import load_dotenv
 import os
 from IPython.display import Markdown
-load_dotenv()
-API_KEY = os.getenv("API_KEY")
-genai.configure(api_key=API_KEY)
-data_path = open("data/data.json")
 
-data = json.load(data_path)
+class Elvy:
+    def __init__(self,query):
+        self.query=query
+        self.model = 'models/embedding-001'
+        load_dotenv()
+        self.API_KEY = os.getenv("API_KEY")
+        genai.configure(api_key=self.API_KEY)
+        self.data_path = open("data/data.json")
+        self.data = json.load(self.data_path)
+        self.df = pd.DataFrame(self.data)
+        self.df.columns = ['Title', 'Text']
 
 
-model = 'models/embedding-001'
-
-df = pd.DataFrame(data)
-df.columns = ['Title', 'Text']
-
-
-query = "Can you tell me five points the Team LiveWires"
-
-def embed_fn(title, text):
-  return genai.embed_content(model=model,
+    def embed_fn(self,title, text):
+        return genai.embed_content(model=self.model,
                              content=text,
                              task_type="retrieval_document",
                              title=title)["embedding"]
 
-df['Embeddings'] = df.apply(lambda row: embed_fn(row['Title'], row['Text']), axis=1)
 
-def find_best_passage(query, dataframe):
-  query_embedding = genai.embed_content(model=model,
-                                        content=query,
-                                        task_type="retrieval_query")
-  dot_products = np.dot(np.stack(dataframe['Embeddings']), query_embedding["embedding"])
-  idx = np.argmax(dot_products)
-  return dataframe.iloc[idx]['Text'] 
+    def find_best_passage(self, dataframe):
+        self.query_embedding = genai.embed_content(model=self.model,
+                                                content=self.query,
+                                                task_type="retrieval_query")
+        dot_products = np.dot(np.stack(dataframe['Embeddings']), self.query_embedding["embedding"])
+        idx = np.argmax(dot_products)
+        return dataframe.iloc[idx]['Text'] 
 
-passage = find_best_passage(query, df)
+    def make_prompt(self,relevant_passage):
+        escaped = relevant_passage.replace("'", "").replace('"', "").replace("\n", " ")
+        prompt = textwrap.dedent("""You are a helpful and informative bot that answers questions using text from the reference passage included below. \
+        Be sure to respond in a complete sentence, being comprehensive, including all relevant background information. \
+        However, you are talking to a non-technical audience, so be sure to break down complicated concepts and \
+        strike a friendly and converstional tone. \
+        If the passage is irrelevant to the answer, you may ignore it.
+        QUESTION: '{query}'
+        PASSAGE: '{relevant_passage}'
 
-def make_prompt(query, relevant_passage):
-  escaped = relevant_passage.replace("'", "").replace('"', "").replace("\n", " ")
-  prompt = textwrap.dedent("""You are a helpful and informative bot that answers questions using text from the reference passage included below. \
-  Be sure to respond in a complete sentence, being comprehensive, including all relevant background information. \
-  However, you are talking to a non-technical audience, so be sure to break down complicated concepts and \
-  strike a friendly and converstional tone. \
-  If the passage is irrelevant to the answer, you may ignore it.
-  QUESTION: '{query}'
-  PASSAGE: '{relevant_passage}'
+            ANSWER:
+        """).format(query=self.query, relevant_passage=escaped)
 
-    ANSWER:
-  """).format(query=query, relevant_passage=escaped)
+        return prompt
 
-  return prompt
-
-
-prompt = make_prompt(query, passage)
+    def answer(self):
+        self.df['Embeddings'] = self.df.apply(lambda row: self.embed_fn(row['Title'], row['Text']), axis=1)
+        self.passage = self.find_best_passage(self.df)
+        self.prompt = self.make_prompt(self.passage)
+        self.model = genai.GenerativeModel('gemini-pro')
+        self.answer = self.model.generate_content(self.prompt)
+        return self.answer.text
+    
+   
 
 
-
-model = genai.GenerativeModel('gemini-pro')
-answer = model.generate_content(prompt)
-
-print(answer.text)
--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from Elvy import Elvy
+from flask import Flask,render_template,request, redirect, url_for, session,jsonify
 
+app = Flask(__name__)
+app.secret_key = 'ELVY_1.0' 
+
+@app.route('/')
+def chat():
+
+    return render_template('bot.html')
+    
+@app.route("/get", methods=["GET", "POST"])
+def chatbot():
+    if request.method == "POST":
+        prompt = request.form["msg"]
+        elvy=Elvy(prompt)
+        response =elvy.answer()
+        return response
+    
+if __name__ == '__main__':
+
+	app.run(debug=True)
