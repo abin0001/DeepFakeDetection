af24fd49da99be11c5059f1816406c48ea484290
abin0001 <abinj2003@gamail.com>
2024-02-17 03:49:46 +0530

    Elvy ui Complete

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 app.secret_key = 'ELVY_1.0' 
 
 @app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/bot')
 def chat():
 
     return render_template('bot.html')
